a9864daf756e63e01e29785dff97f6d693e79c37
gukt <gukaitong@gmail.com>
2021-08-23 18:30:44 +0800

    Update

--- lombok/src/main/java/demos/Person.java
+++ lombok/src/main/java/demos/Person.java
@@ -1,23 +1,24 @@
-package tech.codedog.guides;
+package demos;
 
 import lombok.*;
 
 /**
+ * @author https://github.com/gukt
+ * @version 1.0
  * @Getter @Setter 可以声明在字段和类上。帮我们自动生成 setter、getter @NoArgsConstructor 添加默认构造函数
  * 添加了@Data，会自动添加默认构造函数（注意不会自动添加 RequiredArgsConstructor）、equals/canEqual、hashCode/toString
  *
  *
  * TODO：
  * @RequiredArgsConstructor onConstructor
- *
- *
- * @author https://github.com/gukt
- * @version 1.0
  */
 // @Data
 @NoArgsConstructor(staticName = "of")
 @RequiredArgsConstructor(staticName = "of", access = AccessLevel.MODULE)
 // @Data(staticConstructor="of")
 public class Person {
-  @Getter @Setter @NonNull String name;
+
+    @Getter
+    @Setter
+    @NonNull String name;
 }
